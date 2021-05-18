5fbac241c3dd4425244f0ed30d085e33b6a784e5
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-05-18 10:20:52 -0500

    build initial navbar component

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,55 @@
 import React from 'react'
 import Quote from './Quote'
 
-function Navbar() {
-    return (
-        <div>
-            
+export default function Navbar() {
+  return (
+    <div className="bg-indigo-600">
+      <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between flex-wrap">
+          <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
+            <a
+              href="#"
+              className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50"
+            >
+              Philosophy
+            </a>
+          </div>
+          <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
+            <a
+              href="#"
+              className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50"
+            >
+              Joke
+            </a>
+          </div>
+            <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
+            <a
+              href="#"
+              className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50"
+            >
+              Moral Psychology
+            </a>
+          </div>
+            <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
+            <a
+              href="#"
+              className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50"
+            >
+              Logical Fallacies
+            </a>
+          </div>
+          <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
+            <a
+              href="#"
+              className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50"
+            >
+              Cognitive Biases
+            </a>
+          </div>
+          
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
-export default Navbar
