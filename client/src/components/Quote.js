dc29f120df9e1ce7d185c897e27534549e7f2446
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-05-18 10:12:12 -0500

    style quote

--- client/src/components/Quote.js
+++ client/src/components/Quote.js
@@ -2,23 +2,28 @@ import React from 'react'
 
 function Quote() {
     return (
-        <div className='container mx-auto overflow-auto bg-green-200'>
-            <p className='bg-green-200'>
-                I am le awesome
+        <div className='container mx-auto space-y-6 my-2 bg-green-200 p-2 overflow-auto shadow border-2 border-indigo-200 rounded'>
+            <p className='text-justify'>
+                “It never ceases to amaze me: we all love ourselves more than other people, but care more about their opinion than our own.”
             </p>
-            <span>
-                <button>
-                    Save
-                </button>
-                <button>
-                    Like
-                </button>
-                <button>
-                    Author
-                </button>
-                <button>
-                    Clear
-                </button>
+            <span className='flex space-between center'>
+                <div className='container mx-auto flex space-x-4 content-center'>
+                    <button>
+                        Save
+                    </button>
+                    <button>
+                        Like
+                    </button>
+                    <button>
+                        Author
+                    </button>
+                    <button>
+                        Clear
+                    </button>
+                    <button>
+                        Repeat
+                    </button>
+                </div>
             </span>
         </div>
     )
