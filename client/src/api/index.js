9fd0844fa5e0183bf6152637e17b977ba478f571
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-05-19 12:59:44 -0500

    add draft routes for other types

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,6 +4,13 @@ import axios from 'axios'
 const url = 'http://localhost:5000/quotes'
 const random = 'http://localhost:5000/quotes/random'
 
+//HEY NEW ROUTES
+const joke = 'http://localhost:5000/jokes/random'
+const moralPsych = 'http://localhost:5000/moralPsych/random'
+const fallacies = 'http://localhost:5000/fallacies/random'
+const biases = 'http://localhost:5000/biases/random'
+const facts = 'https://localhost:5000/facts/random'
+
 export const getQuotes = () => axios.get(url)
 export const getQuote = () => axios.get(random)
 export const createQuote = (newQuote) => axios.post(url, newQuote)
