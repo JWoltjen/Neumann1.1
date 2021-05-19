576bd4836c01643c13d0d5ccd5e2f9136880a0fc
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-05-19 13:52:11 -0500

    Add moralPsychology schema in models

---
+++ server/models/MoralPsych.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'; 
+
+const moralSchema = new mongoose.Schema({ 
+    text: String,
+    author: String, 
+})
+
+const Moral = mongoose.model('Moral', moralSchema)
+
+export default Moral
+
+
