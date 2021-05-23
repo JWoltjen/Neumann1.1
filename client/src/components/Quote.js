d8b5b0400e8e4dcb16f3153d8916e003037c8770
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-05-23 11:42:37 -0500

    debug quote not being passed from quotelist as prop

--- client/src/components/Quote.js
+++ client/src/components/Quote.js
@@ -1,25 +1,26 @@
-import React, {useState} from 'react'
+import React, {useState, useContext} from 'react'
+import {QuoteContext} from '../contexts/QuoteContext'
+const Quote = (quote) => {
 
-const Quote = ({setQuotes, quotes, text, name}) => {
-
-    const clearQuote = () => {
-        setQuotes(quotes.filter((quote) => quote.id))
+    const [quotes, setQuotes] = useContext(QuoteContext)
+    const deleteQuote = (id) => {
+        quotes.filter(quote => quote.id !== id)
     }
-    
     return (
         <div className='container mx-auto space-y-6 my-2 bg-green-300 p-2 overflow-auto shadow border-2 border-indigo-200 rounded'>
             <h3 className='text-left font-black'>
-                {name}
+                {quote.name}
             </h3>
             <p className='mx-auto text-justify font-normal'>
-                "{text}"
+                {quote.text}
             </p>
             <span className='flex space-between center'>
                 <div className='container mx-auto flex space-x-4 content-center'>
                     <button>
                         Save
                     </button>
-                    <button onClick={clearQuote}>
+                    <button
+                        onClick={() => deleteQuote(quote.id)}>
                         Clear
                     </button>
                     <button>
